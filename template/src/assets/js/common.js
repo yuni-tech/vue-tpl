6d0a5387c68e7a3e066a981fe141a6c7fde41c31
zhao.hang <zhao.hang@uneed.com>
2018-09-04 17:14:35 +0800

    feat:

--- template/src/assets/js/common.js
+++ template/src/assets/js/common.js
@@ -4,7 +4,8 @@ import '@/assets/css/common.css'
 import FastClick from 'fastclick-fixed'
 FastClick.attach(document.body)
 const setRemUnit = () => {
-    let width = document.documentElement.clientWidth
+    let html = document.documentElement
+    let width = html.clientWidth
     const set_width = width / 3.75
     const now_width = Number(document.documentElement.style.fontSize.slice(0, -2))
     if (set_width != now_width) {
@@ -14,17 +15,17 @@ const setRemUnit = () => {
     // 安卓部分机型rem计算不准,二货安卓
     let whileCount = 0
     while (true) {
-        let settedFs = Number(html.style.fontSize.slice(0, -2))
-        let settingFs = settedFs
+        let settingFs = Number(html.style.fontSize.slice(0, -2))
         let realFs = Number(window.getComputedStyle(html).fontSize.slice(0, -2))
-        let delta = realFs - settedFs
-        if (Math.abs(delta) >= 1) {
+        let delta = realFs - set_width
+        if (Math.abs(delta) >= 0.5) {
             if (delta > 0) {
                 settingFs--
             } else {
                 settingFs++
             }
             html.style.fontSize = settingFs + 'px'
+            // html.style.fontSize = settingFs + 'vw'
         } else {
             break
         }
@@ -33,14 +34,8 @@ const setRemUnit = () => {
         }
     }
 }
-window.onload = () => {
-    let reSet
-    setRemUnit()
-    window.addEventListener('resize', () => {
-        clearTimeout(reSet)
-        reSet = setTimeout(setRemUnit, 300)
-    })
-}
+setRemUnit()
+   
 {{/mobile}}
 
 
