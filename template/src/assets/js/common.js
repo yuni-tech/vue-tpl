4f66a03b032650c8db6461fb4b6667548a0f2110
zhao.hang <zhao.hang@uneed.com>
2018-09-26 12:11:56 +0800

    feat:

--- template/src/assets/js/common.js
+++ template/src/assets/js/common.js
@@ -1,16 +1,16 @@
 import '@/assets/css/base.css'
 {{#mobile}}
 import '@/assets/css/common.css'
-import FastClick from 'fastclick-fixed'
+import FastClick from 'fastclick'
 FastClick.attach(document.body)
 const setRemUnit = () => {
     let html = document.documentElement
+    // html.style.fontSize = 100 / 3.75 + 'vw'
     let width = html.clientWidth
     const set_width = width / 3.75
     const now_width = Number(document.documentElement.style.fontSize.slice(0, -2))
     if (set_width != now_width) {
         document.documentElement.style.fontSize = width / 3.75 + 'px'
-        // document.documentElement.style.fontSize = 100 / 3.75 + 'vw'
     }
     // 安卓部分机型rem计算不准,二货安卓
     let whileCount = 0
@@ -21,8 +21,10 @@ const setRemUnit = () => {
         if (Math.abs(delta) >= 0.5) {
             if (delta > 0) {
                 settingFs--
+                // settingFs = settingFs - 0.3
             } else {
                 settingFs++
+                // settingFs = settingFs + 0.3
             }
             html.style.fontSize = settingFs + 'px'
             // html.style.fontSize = settingFs + 'vw'
